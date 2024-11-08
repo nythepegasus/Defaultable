9f574c2f8ec1aae66f67d56c61edfb2f88b548c2
ny <mac@nythepegas.us>
2024-11-08 08:40:13 -0500

    Add Swift 5.8 to allow Playgrounds

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "Defaultable",
+    products: [
+        .library(
+            name: "Defaultable",
+            targets: ["Defaultable"]),
+        .library(name: "DefaultableFoundation", targets: ["DefaultableFoundation"])
+    ],
+    targets: [
+        .target(
+            name: "Defaultable"),
+        .target(name: "DefaultableFoundation", dependencies: ["Defaultable"]),
+        .testTarget(
+            name: "DefaultableTests",
+            dependencies: ["Defaultable", "DefaultableFoundation"]
+        ),
+    ]
+)
