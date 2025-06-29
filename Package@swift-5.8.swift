21850a8f7ffdf58d07f0587743a0aec22963e014
ny <mac@nythepegas.us>
2025-06-29 06:46:54 -0400

    fix(format): clean up Package.swift

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -5,18 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Defaultable",
     products: [
-        .library(
-            name: "Defaultable",
-            targets: ["Defaultable"]),
+        .library(name: "Defaultable", targets: ["Defaultable"]),
         .library(name: "DefaultableFoundation", targets: ["DefaultableFoundation"])
     ],
     targets: [
-        .target(
-            name: "Defaultable"),
+        .target(name: "Defaultable"),
         .target(name: "DefaultableFoundation", dependencies: ["Defaultable"]),
-        .testTarget(
-            name: "DefaultableTests",
-            dependencies: ["Defaultable", "DefaultableFoundation"]
-        ),
+        .testTarget(name: "DefaultableTests", dependencies: ["Defaultable", "DefaultableFoundation"]),
     ]
 )
