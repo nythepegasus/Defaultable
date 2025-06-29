8a2ec74bc851e263caa9add6c6ef731b5a8ffb8e
ny <mac@nythepegas.us>
2025-06-29 06:45:18 -0400

    fix(tests): general formatting and missing expects

--- Tests/DefaultableTests/DefaultableTests.swift
+++ Tests/DefaultableTests/DefaultableTests.swift
@@ -8,8 +8,8 @@ struct User: Equatable {
     let age: Int?
     
     init(name: String? = nil, age: Int? = nil) {
-        self.name = name~
-        self.age = age~
+        self.name = name
+        self.age = age
     }
 }
 
@@ -22,20 +22,14 @@ struct Post: Equatable {
     let author: User?
     
     init(title: String? = nil, author: User? = nil) {
-        self.title = title~
-        self.author = author~
+        self.title = title
+        self.author = author
     }
 }
 
 extension Post: Defaultable {
     public static var DV: Self { .init() }
 }
-
-extension Equatable {
-    /// For better type checking
-    func same(_ other: Self) -> Bool { self == other }
-}
-
 
 @Suite("Defaultable Sanity Tests", .serialized)
 struct DefaultableSanityTests {
@@ -48,50 +42,64 @@ struct DefaultableSanityTests {
     @Test("Casting Any")
     func castAny() async throws {
         let fetchedData: [String: Any?] = [
-            "name": "Alice",
-            "age": 25,
+            "name": "ny :3",
+            "age": 84,
             "email": nil
         ]
         for (key, value) in fetchedData {
-            if value == nil {
-                if key.same("email") { print(value ~~ String.self) }
-            }
+            if key == "email" { let v: String = value~; #expect(v == .DV); print("email: \(v)") }
+            if key == "age" { let v: Int = value~; #expect(84 == v); print("age: \(v)") }
+            if key == "name" { let v: String = value~; #expect("ny :3" == v); print("name: \(v)") }
         }
+    }
+
+    @Test("Casting String from Any?")
+    func castAnyOptionalString() throws {
+        func buh() -> Any? { "buh" }
+        let s: String = buh()~
+        #expect(s == "buh")
+    }
+
+    @Test("Casting Int from Any")
+    func castAnyInt() throws {
+        func buh() -> Any { 84 }
+        let s: Int = buh()~
+        #expect(s == 84)
     }
     
     @Test("Simple struct")
     func nyUser() async throws {
-        #expect("ny".same(ny.name~))
-        #expect(84.same(ny.age~))
+        #expect("ny" == ny.name~)
+        #expect(84 == ny.age~)
     }
     
     @Test("Nested struct")
     func nyPost() async throws {
-        #expect("hello".same(post.title~))
-        #expect(ny.same(post.author~))
+        #expect("hello" == post.title~)
+        #expect(ny == post.author~)
     }
     
     @Test("nil struct")
     func nilStruct() async throws {
         let u: User? = nil
-        #expect(u.same(nil))
-        #expect(User.DV.same(u~))
-        #expect("".same(u~.name~))
-        #expect(0.same(u~.age~))
+        #expect(u == nil)
+        #expect(u~ == .DV)
+        #expect("" == u~.name~)
+        #expect(0 == u~.age~)
     }
     
     @Test("nil Nested struct")
     func nilNestedStruct() async throws {
         let p: Post? = nil
-        #expect(p.same(nil))
-        #expect(Post.DV.same(p~))
-        #expect("".same(p~.title~))
-        #expect(User.DV.same(p~.author~))
+        #expect(p == nil)
+        #expect(p~ == .DV)
+        #expect(p~.title~ == .DV)
+        #expect(p~.author~ == .DV)
     }
     
     @Test("ascii bytes")
     func asciiBytes() async throws {
         let ny: [UInt8] = [78, 121]
-        print(String(bytes: ny, encoding: .ascii))
+        #expect("Ny" == String(bytes: ny, encoding: .ascii)~)
     }
 }
